adefd6fa1e464160f989ad92feb90d5c5b1b1cab
ndow-cap <nldow@mit.edu>
2023-04-19 20:24:59 +0000

    look ahead intersection implemented but not tested

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -50,6 +50,7 @@ class PurePursuit(object):
         self.trajectory.fromPoseArray(msg)
         self.trajectory.publish_viz(duration=0.0)
 
+        # might want to extract pose directions as well? or is just x,y sufficient
         for pose in msg.poses:
             x = pose.position.x
             y = pose.position.y
@@ -59,9 +60,6 @@ class PurePursuit(object):
         self.x_values = np.array(self.x_values)
         self.y_values = np.array(self.y_values)
 
-        
-
-
     def odometry_callback(self, odom):
         px = odom.pose.pose.position.x
         py = odom.pose.pose.position.y
@@ -70,6 +68,55 @@ class PurePursuit(object):
         print('x_values', self.x_values)
         print('y_values', self.y_values)
         np.array([self.distance(self.x_values,self.y_values,p_i) for p_i in p])
+
+    def lookahead_intersection(self, pose, segment):
+        """
+        Takes in pose as Odometry msg and a line segment index in the list of 
+        poses for the trajectory. Returns intersection point of the closest trajectory
+        that intersects with a circle of radius `self.lookahead_dist` centered on
+        pose.
+        Intersection equation: 
+        https://codereview.stackexchange.com/questions/86421/line-segment-to-circle-collision-algorithm/86428#86428
+        """
+        center = np.array(([pose.pose.pose.position.x, pose.pose.pose.position.y]))
+        r = self.lookahead
+        intersection = False
+        current_segment = segment
+        # We iterate through subsequent line segments after closest until intersection
+        while (not intersection and current_segment < self.x_values.size - 1):
+            seg_start = np.array([self.x_values[current_segment], self.y_values[current_segment]])
+            seg_v = np.array([self.x_values[current_segment+1], self.y_values[current_segment+1]]) - seg_start
+            current_segment += 1
+            a = np.dot(seg_v, seg_v)
+            b = 2 * np.dot(seg_v, seg_start - center)
+            c = np.dot(seg_start, seg_start) + np.dot(center, center) - 2 * np.dot(seg_start, center) - r**2
+            disc = b**2 - 4 * a * c
+            if disc < 0:
+                # no solution, continue to next segment
+                continue
+            sqrt_disc = np.sqrt(disc)
+            t1 = (-b + sqrt_disc) / (2 * a)
+            t2 = (-b - sqrt_disc) / (2 * a)
+            if not (0 <= t1 <= 1 or 0 <= t2 <= 1):
+                # intersection outside of line segment, continue
+                continue
+            soln_param = t1
+            if (0 <= t1 <= 1 and 0 <= t2 <= 1):
+                # edge case, currently return t1
+                print("edge case!")
+            else (0 <= t2 <= 1):
+                # t2 is our intersection point
+                soln_param = t2
+        return seg_start + soln_param * seg_v
+        
+
+
+
+
+
+
+
+
 
         
 
