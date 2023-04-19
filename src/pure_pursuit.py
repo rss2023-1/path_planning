94155f84a774e12effd90f0bdc3d242ed55b9d51
ndow-cap <nldow@mit.edu>
2023-04-19 16:36:34 +0000

    setting up pure pursuit

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -16,12 +16,13 @@ class PurePursuit(object):
     """
     def __init__(self):
         self.odom_topic       = rospy.get_param("~odom_topic")
-        self.lookahead        = # FILL IN #
-        self.speed            = # FILL IN #
-        self.wheelbase_length = # FILL IN #
+        self.lookahead        = rospy.get_param("~lookahead_dist")
+        self.speed            = rospy.get_param("~pursuit_speed")
+        self.wheelbase_length = 0.2 #measure this for sure
         self.trajectory  = utils.LineTrajectory("/followed_trajectory")
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
+        self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback, queue_size = 1)
 
     def trajectory_callback(self, msg):
         ''' Clears the currently followed trajectory, and loads the new one from the message
@@ -30,6 +31,13 @@ class PurePursuit(object):
         self.trajectory.clear()
         self.trajectory.fromPoseArray(msg)
         self.trajectory.publish_viz(duration=0.0)
+
+    def odom_callback(self, msg):
+        # Reset a timer for assessing how long it's been since particle filter published/failed?
+
+        # Do lookahead distance intersection
+
+        # Publish Drive topic
 
 
 if __name__=="__main__":
