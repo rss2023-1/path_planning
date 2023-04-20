0e2084476c57a2f197ad22098c3ad8e2128e43d5
danegleason <gleasond@mit.edu>
2023-04-20 15:13:05 +0000

    vectorized min dists

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -15,9 +15,9 @@ class PurePursuit(object):
     """ Implements Pure Pursuit trajectory tracking with a fixed lookahead and speed.
     """
     def __init__(self):
-        self.odom_topic       = rospy.get_param("~odom_topic")
-        self.lookahead        = rospy.get_param("~lookahead_dist")
-        self.speed            = rospy.get_param("~pursuit_speed")
+        self.odom_topic       = rospy.get_param("~odom_topic", "pf/pose/odom")
+        self.lookahead        = rospy.get_param("~lookahead_dist", 0.5)
+        self.speed            = rospy.get_param("~pursuit_speed", 0.5)
         self.wheelbase_length = 0.2 #measure this for sure
         self.trajectory  = utils.LineTrajectory("/followed_trajectory")
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
@@ -25,21 +25,6 @@ class PurePursuit(object):
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
         self.x_values = []
         self.y_values = []
-
-
-    def distance(self, P0, P1, p):
-        # Calculate the vector T and V
-        T = P1 - P0
-        V = p - P0
-        # Calculate the scalar L and U using dot product
-        L = np.sum(T**2, axis=1)
-        U = np.sum(T*V, axis=1)/L
-        # Clip U to ensure it is within the bounds of [0,1]
-        U = np.clip(U, 0, 1)
-        # Calculate C using vector T and scalar U
-        C = P0 + U[:,np.newaxis]*T
-        # Calculate the distance between C and p using norm function
-        return np.linalg.norm(C - p, axis=1)
 
 
     def trajectory_callback(self, msg):
@@ -50,6 +35,7 @@ class PurePursuit(object):
         self.trajectory.fromPoseArray(msg)
         self.trajectory.publish_viz(duration=0.0)
 
+        # Read trajectory x and y values when initialized
         for pose in msg.poses:
             x = pose.position.x
             y = pose.position.y
@@ -59,17 +45,50 @@ class PurePursuit(object):
         self.x_values = np.array(self.x_values)
         self.y_values = np.array(self.y_values)
 
-        
-
 
     def odometry_callback(self, odom):
-        px = odom.pose.pose.position.x
-        py = odom.pose.pose.position.y
-        p = np.array([px, py])
-        print('odom', odom)
-        print('x_values', self.x_values)
-        print('y_values', self.y_values)
-        np.array([self.distance(self.x_values,self.y_values,p_i) for p_i in p])
+        # Get path coords from traj initialization
+        path_coords = np.vstack((self.x_values, self.y_values))
+        # Initialize vector containing all line segment points
+        pairs = np.zeros((2, 2*(path_coords.shape[1]-1)))
+        pairs[:, ::2] = path_coords[:, :-1]
+        pairs[:, 1::2] = path_coords[:, 1:]
+
+        # pairs = [p1, p2, p2, p3, p3, p4, ...] where p is 2x1 [x y]'
+        # for indexing, want line_segs = [[p1, p2], [p2, p3], [p3, p4], ...]
+
+        # Group pairs into line segments
+        pairst = np.transpose(pairs)
+        line_segs = pairst.reshape(-1, 2, 2)
+
+        # Get baselink position as x, y coord
+        xpos = odom.pose.pose.position.x
+        ypos = odom.pose.pose.position.y
+        point = np.array([xpos, ypos])
+        
+        # Calculate minimum distances between current position and each line segment
+        # Initialize array
+        min_distances = np.zeros(line_segs.shape[0])
+        # Loop over each line segment
+        for i in range(line_segs.shape[0]):
+            # Get the starting and ending points of the line segment
+            p1 = line_segs[i, 0]
+            p2 = line_segs[i, 1]
+            # Compute the vector representing the line segment
+            v = p2 - p1
+            # Compute the vector from the starting point of the line segment to the point
+            w = point - p1
+            # Compute the projection of w onto v
+            proj = np.dot(w, v) / np.dot(v, v) * v
+            # Compute the distance between the point and the projected point
+            dist = np.linalg.norm(proj - w)
+            # If the projection is outside the line segment, compute the distance to the closest endpoint
+            if np.dot(proj - p1, proj - p2) > 0:
+                dist = min(np.linalg.norm(point - p1), np.linalg.norm(point - p2))
+            # Store the minimum distance for this line segment
+            min_distances[i] = dist
+        
+        print('min_dists', min_distances)
 
         
 
